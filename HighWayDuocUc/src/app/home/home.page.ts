176bb2f0fc330c9f6c54513bf4118f36d9b80aae
Ruben14f <mansilla8090@gmail.com>
2024-08-31 00:12:33 -0400

    inicio de sesion rayo macuin cushau

--- HighWayDuocUc/src/app/home/home.page.ts
+++ HighWayDuocUc/src/app/home/home.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  password: string = ''; // Aqui se almacena el valor que se ingreso en el campo para contraseña
+  passwordType: string = 'password'; // tipo del campo de entrada de la contraseña (Oculta en este caso por los *****)
 
   constructor() {}
 
+  togglePasswordVisibility() {
+    // Alterna el tipo de campo entre password y text, Si passwordType es password cambiara a text y esto hara que sea visible la contraseña, de lo contrario sera password y la ocultara
+    this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+  }
 }
