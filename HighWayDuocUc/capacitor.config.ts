3cd2ab70b78c3a03abf5e9c8c9ee335b60a852ca
Vincentiko <seb.benavides.quezada@gmail.com>
2024-11-16 18:50:10 -0300

    appId generado

--- HighWayDuocUc/capacitor.config.ts
+++ HighWayDuocUc/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.duocuc.highway',
   appName: 'HighWayDuocUc',
   webDir: 'www'
 };
