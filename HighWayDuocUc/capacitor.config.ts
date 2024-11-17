c9c039aa4e2080c3a8426567d984f3eb45d979b5
Vincentiko <seb.benavides.quezada@gmail.com>
2024-11-17 00:03:24 -0300

    Pico idea que le pasaaa

--- HighWayDuocUc/capacitor.config.ts
+++ HighWayDuocUc/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.duocuc.highway',
   appName: 'HighWayDuocUc',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  android: {
+    webContentsDebuggingEnabled: true
+  }
 };
 
 export default config;
